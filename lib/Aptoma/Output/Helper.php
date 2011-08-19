a1f966010445d22a7f277fdfe5dbd1ef048b411a
Martin Jonsson <martin.jonsson@gmail.com>
2011-08-19 08:45:22 +0200

    Updated to support forcing of format in the controller with $this->_setParam('format')

--- lib/Aptoma/Output/Helper.php
+++ lib/Aptoma/Output/Helper.php
@@ -1,27 +1,50 @@
 <?php
 class Aptoma_Output_Helper extends Zend_Controller_Action_Helper_Abstract
 {
-    public function postDispatch()
+    public function preDispatch()
     {
 		$outputFormat = $this->getRequest()->getParam('format', 'html');
+	   	$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+
 		switch ($outputFormat) {
 			case 'json':
 				$this->disableLayout();
-	    		$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
 				$viewRenderer->setNoRender(true);
-
-				$this->sendJSON($viewRenderer->view->getVars());
 				break;
-			case 'ajax';
+			case 'ajax':
 				$this->disableLayout();
 				break;
+
+			case 'html':
+				$viewRenderer->setNoRender(false);
+				$this->enableLayout();
+				break;
 		}
+    }
+
+    public function postDispatch() {
+    	$this->preDispatch();
+
+    	$outputFormat = $this->getRequest()->getParam('format', 'html');
+		switch ($outputFormat) {
+			case 'json':
+				$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+				$this->sendJSON($viewRenderer->view->getVars());
+				break;
+		}
+
     }
 
     private function disableLayout()
     {
 		$layout = Zend_Layout::getMvcInstance();
 		$layout->disableLayout();
+    }
+
+    private function enableLayout()
+    {
+		$layout = Zend_Layout::getMvcInstance();
+		$layout->enableLayout();
     }
 
     private function sendJSON($data)
