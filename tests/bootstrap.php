c2aaeda9bb0c267526c0474b0b4204c4516f7c9d
Martin Jonsson <martin.jonsson@gmail.com>
2011-11-02 15:03:24 +0100

    fix double include problem

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
 )));
 
-include "Zend/Loader/Autoloader.php";
+require_once("Zend/Loader/Autoloader.php");
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->registerNamespace('Aptoma_');
 $autoloader->registerNamespace('DF_');
