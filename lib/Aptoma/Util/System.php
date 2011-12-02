2c375c190ff8f63f00b05396f07e606f27e6625d
Patrik Henningsson <patrik.henningsson@gmail.com>
2011-12-02 10:13:28 +0100

    Updated Aptoma_Util_System::execCommand() to handle exitcode more flexible

--- lib/Aptoma/Util/System.php
+++ lib/Aptoma/Util/System.php
@@ -5,24 +5,33 @@ class Aptoma_Util_System
 	/**
 	* Wrapper for php exec command.
 	*
-	* @param $command String - the command line to exec.
+	* @param $command string - The command that will be executed.
+	* @param $exitcode int - If argument is present, then the return status of the executed command will be written to this variable.
 	* @return String output from the command.
 	* @throws Aptoma_Util_SystemException if the exec return value wasnt 0. The exception error code will be set to the return value of the exec command.
 	*/
-	public static function execCommand($command)
+	public static function execCommand($command, &$exitcode = null)
 	{
 		$output = array();
-		$returnValue = -1;
 
-		exec($command, $output, $returnValue);
+		exec($command, $output, $exitcode);
+
 		$output = implode("\n", $output);
 
-		if ($returnValue !== 0) {
-			if ($returnValue == 127) {
-				throw new Aptoma_Util_SystemException('execCommand(' . $command . '): command not found.', $returnValue);
-			}
+		switch ($exitcode) {
+			case 2:
+				throw new Aptoma_Util_SystemException('Incorrect usage (cmd = ' . $command  . ')', $exitcode);
+				break;
+			case 126:
+				throw new Aptoma_Util_SystemException('Command found but is not executable (cmd = ' . $command  . ')', $exitcode);
+				break;
+			case 127:
+				throw new Aptoma_Util_SystemException('Command not found (cmd = ' . $command  . ')', $exitcode);
+				break;
+		}
 
-			throw new Aptoma_Util_SystemException('execCommand(' . $command. ") did not exit properly.\nExit code was: " . $returnValue . "\nOutput: " . $output, $returnValue);
+		if ($exitcode !== 0) {
+			throw new Aptoma_Util_SystemException('Command did not exit properly (cmd = ' . $command  . ') (code = ' . $exitcode . ') output: ' . $output, $exitcode);
 		}
 
 		return $output;
